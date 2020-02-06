6582ffabd3ca44ba7d6f536e03d4a1f336a0ecea
JohnGfrusciante <g.rejime.frusciante@gmail.com>
2020-02-07 03:31:42 +0900

    TOPページ表示の為のファイル各種ファイル作成・編集

--- app/controllers/matters_controller.rb
+++ app/controllers/matters_controller.rb
@@ -1,2 +1,4 @@
 class MattersController < ApplicationController
+  def index
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+    }
+
+    devise_scope :user do
+      get 'basic', to: 'users/registrations#basic'
+      post 'basic', to: 'users/registrations#basic'
+      get 'performance', to: 'users/registrations#performance'
+      post 'performance', to: 'users/registrations#performance'
+      get 'logout', to: "users#logout"
+    end
+    root to: "matters#index"
+
+    resources :matters
+
 end
