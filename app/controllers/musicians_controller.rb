ead31089c167e6c8a0d0dedabd2c27588a632521
JohnGfrusciante <g.rejime.frusciante@gmail.com>
2020-02-10 01:06:34 +0900

    アーティスト登録に必要なファイルの作成と編集
    画像と音源データがアップできないので、引き続き修正が必要ですが、必須情報のDB保存はできたので、コミットします。

--- app/controllers/musicians_controller.rb
+++ app/controllers/musicians_controller.rb
@@ -1,2 +1,36 @@
 class MusiciansController < ApplicationController
+  def index
+  end
+
+  def new
+    @user_id = current_user.id
+    if @user_id.present?
+      # redirect_to root_path
+      # ===ここからテスト用記述===================================
+      @musician = Musician.new
+      # テスト用記述は、アーティスト登録終了後に削除し、redirect_to root_path のコメントアウトを外してください。
+      # また、musician.brのモデルファイルの validates :user_id, uniqueness: true のコメントアウトも外してください。
+      # ===ここまでテスト用記述===================================
+    else
+      @musician = Musician.new
+    end
+  end
+
+  def create
+    # @musician = Musician.create(musician_params)
+    @musician = Musician.new(musician_params)
+    @musician.save!
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+  def musician_params
+    params.require(:musician).permit(:image, :name, :biography, :activity_history, :activity_place, :email, :sound_source, :homepage, :twitter, :facebook, :other_link, :live_info, :office).merge(user_id: current_user.id)
+  end
+
 end
--- app/models/musician.rb
+++ app/models/musician.rb
@@ -1,4 +1,6 @@
 class Musician < ApplicationRecord
+  validates :name, :biography, :activity_history, :activity_place, :email ,presence: true
+  # validates :user_id, uniqueness: true
   has_many :matters
   has_many :chats
   has_many :messages
