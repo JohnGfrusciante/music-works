54c92a15b583b2527003ad38a4e912404aa71d4b
JohnGfrusciante <g.rejime.frusciante@gmail.com>
2020-02-15 14:56:16 +0900

    メッセージ送信機能実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,17 @@
 class MessagesController < ApplicationController
 
+  before_action :set_matter, only: [:index, :create, :edit, :update, :destroy]
+  before_action :set_chat, only: [:index, :create, :edit, :update, :destroy]
+
   def index
-    @matter = Matter.find(params[:matter_id])
-    @chat = Chat.find_by(matter_id: @matter.id, user_id: current_user.id)
+    @message = Message.new
+    @messages = Message.all.order("created_at DESC")
   end
 
   def create
-    
+    @message = Message.new(message_params)
+    @message.save!
+    redirect_to controller: :messages, action: :index
   end
 
   def edit
@@ -19,6 +24,20 @@ class MessagesController < ApplicationController
 
   def destroy
     
+  end
+
+
+  private
+  def message_params
+    params.require(:message).permit(:content, :status).merge(user_id: current_user.id, chat_id: @chat.id)
+  end
+
+  def set_matter
+    @matter = Matter.find(params[:matter_id])
+  end
+
+  def set_chat
+    @chat = Chat.find_by(matter_id: @matter.id, user_id: current_user.id)
   end
 
 end
