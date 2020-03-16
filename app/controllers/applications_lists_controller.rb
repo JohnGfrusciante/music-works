67172bd4af4bd683538c8d1a3a590b55be1fa525
JohnGfrusciante <g.rejime.frusciante@gmail.com>
2020-03-16 23:05:37 +0900

    応募案件一覧ページマークアップ

--- app/controllers/applications_lists_controller.rb
+++ app/controllers/applications_lists_controller.rb
@@ -2,6 +2,7 @@ class ApplicationsListsController < ApplicationController
 
   def index
     @applications = Application.where(user_id: current_user.id).order("created_at DESC")
+    @musician_user_id = Musician.find_by(user_id: current_user)
   end
 
 end
