318fe34835527c232bba080b7d4ece02b2b6d9ad
JohnGfrusciante <g.rejime.frusciante@gmail.com>
2020-03-19 18:54:46 +0900

    ユーザースキル検索ページのマークアップ

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -7,6 +7,7 @@ class TagsController < ApplicationController
 
   def user_search
     @user_skill_searchs = Tag.where(skill_id: params[:skill_id])
+    @musician_user_id = Musician.find_by(user_id: current_user)
   end
 
 
