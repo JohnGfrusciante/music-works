425a6e741011b5cdc8626e326972e3b20e342454
JohnGfrusciante <g.rejime.frusciante@gmail.com>
2020-03-20 19:46:36 +0900

    案件応募結果一覧ページのマークアップ

--- app/controllers/musician_matterslists_controller.rb
+++ app/controllers/musician_matterslists_controller.rb
@@ -1,6 +1,6 @@
 class MusicianMatterslistsController < ApplicationController
   def index
     @matters = Matter.where(musician_id: current_user.musician.id).order("created_at DESC")
+    @musician_user_id = Musician.find_by(user_id: current_user)
   end
 end
-
