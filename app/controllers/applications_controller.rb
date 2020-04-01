8390ac71e74dd1cca7a5cf1f183967473ff3d0d2
JohnGfrusciante <g.rejime.frusciante@gmail.com>
2020-04-01 16:17:38 +0900

    案件応募ページ、応募取り消しページのマークアップ

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -2,11 +2,15 @@ class ApplicationsController < ApplicationController
 
   def create
     current_user.applications.create(matter_id: params[:matter_id])
+    @matter = Matter.find_by(id: params[:matter_id])
+    @musician_user_id = Musician.find_by(user_id: current_user)
   end
 
   def destroy
     @application = Application.find_by(matter_id: params[:matter_id], user_id: current_user.id)
     @application.destroy
+    @matter = Matter.find_by(id: params[:matter_id])
+    @musician_user_id = Musician.find_by(user_id: current_user)
   end
 
 end
